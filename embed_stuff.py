02af152d5dd6e561be6d9b40dc89f7b96acd8175
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-12-03 22:17:26 -0800

    removed extra stuff

--- embed_stuff.py
+++ embed_stuff.py
@@ -5,40 +5,9 @@ import lgpio
 
 PWM_OUT = 12
 
-# import lgpio
-# def spin_motor(clockwise = True, delay = 0.004, step_max = 4096, con = [17, 27, 22, 23]):
-#     #Half-step, so step_max = 4096 for full rotation
-#     steps = [
-#         0b1001,
-#         0b1000,
-#         0b1100,
-#         0b0100,
-#         0b0110,
-#         0b0010,
-#         0b0011,
-#         0b0001
-#     ]
-
-#     h = lgpio.gpiochip_open(0)
-#     lgpio.group_claim_output(h, con)
-
-#     try:
-#         step = 0
-#         while command.empty():
-#             lgpio.group_write(h, con[0], steps[step])
-#             step = (step - 1) % 8 if clockwise else (step + 1) % 8
-#             sleep(delay)
-#     except Exception:
-#         lgpio.group_write(h, con[0], 0)
-#         lgpio.group_free(h, con[0])
-#         lgpio.gpiochip_close(h)
-#         exit(1)
-
 def spin_fan():
-    global prev_temp
     t_dif = cur_temp - goal_temp
 
-    #Don't call pwm stuff if not a big difference
     # if (cur_temp > prev_temp - 0.5 and cur_temp < prev_temp + 0.5):
     #     return
     # if t_dif < 0:
@@ -59,7 +28,6 @@ def spin_fan():
         lgpio.tx_pwm(h_pwm, PWM_OUT, 2, 50)
     else:
         lgpio.tx_pwm(h_pwm, PWM_OUT, 10, 50) 
-    prev_temp = cur_temp
 
 def do_stuff():
     while enable:
@@ -80,14 +48,13 @@ def stop():
     lgpio.gpio_free(h_pwm, PWM_OUT)
 
 def init():
-    global doc_root, goal_temp, cur_temp, h_pwm, prev_temp
+    global doc_root, goal_temp, cur_temp, h_pwm
     doc_root = os.path.dirname(__file__)
     with open(doc_root + "/goal_temp.txt","r") as f:
         goal_temp = float(f.read())
     cur_temp = 30.1
     h_pwm = lgpio.gpiochip_open(0)
     lgpio.gpio_claim_output(h_pwm, PWM_OUT)
-    prev_temp = 0
     start()
 
 if __name__ == "__main__":
